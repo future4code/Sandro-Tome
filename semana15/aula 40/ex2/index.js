67d3571989af2ae8117db00f56da29086ef42af5
Sandro-Tome <sandrotome.jr@gmail.com>
2021-03-15 16:18:41 -0300

    Exercícios feitos

--- semana15/aula 40/ex2/index.js
+++ semana15/aula 40/ex2/index.js
@@ -1,18 +1,3 @@
-/* Exercício 1: */
-
-//a) Para acessar os parâmetros passados na linha de comando utilizamos a propriedade process.argv 
-
-//b)
-
-const name = process.argv[2]
-const age = Number(process.argv[3])
-
-console.log(`Olá ${name}! Você tem ${age} anos.`)
-
-//c)
-
-console.log(`Olá ${name}! Você tem ${age} anos. Em sete anos você terá ${age + 7}.`)
-
 /* Exercício 2: */
 
 const operacao = process.argv[2]
@@ -35,10 +20,3 @@ switch (operacao) {
   default:
     console.log("Operação inválida");
 }
-
-/* Exercício 3 */
-
-const taskList = []
-const task = taskList.push(process.argv[2])
-
-console.log(taskList)
